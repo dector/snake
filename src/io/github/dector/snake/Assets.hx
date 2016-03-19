c9084b2da1bf6f24cf8ee14e5ffb56caa4982edc
Denys M <dector9@gmail.com>
2016-03-19 17:37:39 +0200

    Add PlayState

---
+++ src/io/github/dector/snake/Assets.hx
@@ -0,0 +1,9 @@
+package io.github.dector.snake;
+
+class Assets {
+
+    public static inline var MUSIC_TRACK1 = "assets/music/track1.ogg";
+
+    public static inline var SOUND_EAT = "assets/sounds/eat.wav";
+    public static inline var SOUND_GAME_OVER = "assets/sounds/game_over.wav";
+}
